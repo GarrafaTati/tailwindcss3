75cc422f50d859fee7e73aff1fecbe70a1c04603
GarrafaTati <tatiigarrafa@gmail.com>
2023-08-08 16:03:17 -0400

    cleaning files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
-import "./styles/pages/index.css";
-import stepsSrc from "./images/steps.png";
-
-const numbers = [2, 3, 5];
-
-// Função arrow. Como o Internet Explorer lidará com isso?
-const doubledNumbers = numbers.map((number) => number * 2);
-
-console.log(doubledNumbers); // 4, 6, 10
-
-const stepsImage = document.getElementById("image-steps");
-stepsImage.src = stepsSrc;
+import "./assets/styles/index.css";
