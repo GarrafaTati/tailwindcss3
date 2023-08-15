a8b31a36316cd037ecf806b6e6d1cf71ce95d4d1
GarrafaTati <tatiigarrafa@gmail.com>
2023-08-15 15:19:05 -0400

    new config lineHeight, header, add icons

--- tailwindcss3/tailwind.config.js
+++ tailwindcss3/tailwind.config.js
@@ -3,6 +3,14 @@ export default {
   content: ["./src/index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontSize: {
+      "2xl": [
+        "1.5rem",
+        {
+          lineHeight: "1.5rem",
+        },
+      ],
+    },
   },
   plugins: [],
 };
